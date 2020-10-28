ff03da7564084e29ac2c7bb769ff5c6cd79725ce
siyile <seayahh@outlook.com>
2020-10-28 00:46:35 -0700

    Successfully test CORS

--- packages/website/src/Home.tsx
+++ packages/website/src/Home.tsx
@@ -1,12 +1,25 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { Button, Typography } from '@material-ui/core'
+import { fetchCounter } from './api'
 
-function Home(): ReactElement {
+const Home = (): ReactElement => {
+  const [value, setValue] = useState('This is Material-UI')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const counterData = await fetchCounter()
+      setValue(counterData)
+    }
+    fetchData()
+      .then((r) => console.log('Fetched'))
+      .catch((e) => console.log('Failed'))
+  }, [])
+
   return (
     <>
       <Typography variant="h1">Hi, there</Typography>
       <Button variant="contained" color="primary">
-        This is Material-UI
+        {value}
       </Button>
     </>
   )
